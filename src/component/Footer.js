e5c5179c5a4b4b22188d53fcd14b6e9b34c41bf1
pgrandne <perrin.grandne@protonmail.com>
2022-04-20 09:53:00 +0200

    new url for contact

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -10,7 +10,7 @@ const Footer = ({ t }) => {
 				</p>
 				<ul>
 					<li><Link to="/mentions">{t('Mentions légales')}</Link></li>
-					<li><a href="https://calendly.com/mathieu-strabach-menjs/test-usabilite?month=2022-03">{t('Contactez-nous')}</a></li>
+					<li><a href="https://calendly.com/mathieu-strabach-menjs/dases?month=2022-04">{t('Contactez-nous')}</a></li>
 					<li><a href="https://github.com/pgrandne/daseslab">GitHub</a></li>
 				</ul>
 			</div>
