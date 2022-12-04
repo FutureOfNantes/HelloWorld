3e3467458b05b881fc6bd25c624fb37af97c7498
Perrin <perrin.grandne@protonmail.com>
2022-12-04 23:08:42 +0100

    update modules

--- client/src/page/VerifiableCredential.js
+++ client/src/page/VerifiableCredential.js
@@ -1,6 +1,6 @@
 import { useDispatch, useSelector } from 'react-redux'
 import { addAsyncUser } from '../features/reducers/userSlice';
-import { Fragment, useState } from 'react';
+import { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { Link, useNavigate } from 'react-router-dom';
 import sign from '../features/sign';
@@ -70,95 +70,95 @@ const VerifiableCredential = ({ t }) => {
                 <li className="back"><button onClick={() => { navigate(-1) }}>{t('retour')}</button></li>
             </ul>
             <div>
-                            {!vcDone &&
-                                <section className="main onboardingStep onboardingUserInfos">
-                                    <h1>
-                                        {t('Obtenez votre laissez-passer')}
-                                    </h1>
-                                    <p>{t('DasesLab met en relation')}</p>
-                                    <input className="publicKey" {...register("did")} value={did} readOnly />
-                                    <form className="form" onSubmit={handleSubmit(onSubmit)}>
-                                        <div className="formGroup field">
-                                            <input className="formField" {...register("familyName")} />
-                                            <label htmlFor="name" className="formLabel">
-                                                {t('Votre nom')}
-                                            </label>
-                                        </div>
-                                        <div className="formGroup field">
-                                            <input className="formField" {...register("givenName")} />
-                                            <label htmlFor="forname" className="formLabel">
-                                                {t('Votre prénom')}
-                                            </label>
-                                        </div>
-                                        <div className="formGroup field">
-                                            <input type="email" className="formField" {...register("emailOwner")} />
-                                            <label htmlFor="proEmail" className="formLabel">
-                                                {t('Votre courriel professionnel')}
-                                            </label>
-                                        </div>
-                                        <div className="formGroup field">
-                                            <input className="formField" {...register("companyName")} />
-                                            <label htmlFor="entityName" className="formLabel">
-                                                {t('Nom de votre organisation')}
-                                            </label>
-                                        </div>
-                                        <div className="formGroup field">
-                                            <input className="formField" {...register("companyAddress")} />
-                                            <label htmlFor="proAddress" className="formLabel">
-                                                {t('Adresse de votre organisation')}
-                                            </label>
-                                        </div>
-                                        <div className="formGroup">
-                                            <input type="checkbox" id="consentCGV" onChange={handleLegalRep} />
-                                            <label htmlFor="consentCGV">
-                                                {t("Je ne suis pas le représentant légal de l'organisation")}
-                                            </label>
-                                        </div>
-                                        {!legalRep && <div className="formGroup field">
-                                            <input className="formField" required {...register("legalRepresentative")} />
-                                            <label htmlFor="name" className="formLabel">
-                                                {t('Nom du représentant légal')}
-                                            </label>
-                                        </div>}
-                                        {!legalRep && <div className="formGroup field">
-                                            <input type="email" className="formField" {...register("emailLegalRepresentative")} />
-                                            <label htmlFor="proEmail" className="formLabel">
-                                                {t('Courriel du représentant légal')}
-                                            </label>
-                                        </div>}
-                                        <div className="formGroup">
-                                            <input type="checkbox" id="consentCGV" required />
-                                            <label htmlFor="consentCGV">
-                                                {t("Je consens à rejoindre DasesLab, et m’engage à respecter sa")}
-                                                <Link to="/mentions">
-                                                    {t("charte d'utilisation")}
-                                                </Link>
-                                            </label>
-                                        </div>
-                                        <input type="submit" className="button blackButton" value="Confirmer mon identité" />
-                                    </form>
-                                </section>}
-                            {!!vcDone && <section className="main onboardingStep onboardingUserInfos">
-                                <button className="closeButton fixed" onClick={handleClose}>
-                                    {t('Fermer')}
-                                </button>
-                                <h1>
-                                    {t('Votre laissez-passer est créé')}
-                                </h1>
-                                <p>{t("Vous pouvez maintenant utiliser les services du portail")}</p>
-                                <button className="button blackButton" onClick={handleClose}>
-                                    {t('Retourner sur le Dashboard')}
-                                </button>
-                                <p /><p />
-                                <p>{t('Vous pouvez télécharger votre laissez-passer pour le conserver en local')}</p>
-                                <button className="button blackButton" onClick={handleVc}>
-                                    {t('Télécharger votre laissez-passer')}
-                                </button>
-                            </section>}
-                </div>
-                </section>
+                {!vcDone &&
+                    <section className="main onboardingStep onboardingUserInfos">
+                        <h1>
+                            {t('Obtenez votre laissez-passer')}
+                        </h1>
+                        <p>{t('DasesLab met en relation')}</p>
+                        <input className="publicKey" {...register("did")} value={did} readOnly />
+                        <form className="form" onSubmit={handleSubmit(onSubmit)}>
+                            <div className="formGroup field">
+                                <input className="formField" {...register("familyName")} />
+                                <label htmlFor="name" className="formLabel">
+                                    {t('Votre nom')}
+                                </label>
+                            </div>
+                            <div className="formGroup field">
+                                <input className="formField" {...register("givenName")} />
+                                <label htmlFor="forname" className="formLabel">
+                                    {t('Votre prénom')}
+                                </label>
+                            </div>
+                            <div className="formGroup field">
+                                <input type="email" className="formField" {...register("emailOwner")} />
+                                <label htmlFor="proEmail" className="formLabel">
+                                    {t('Votre courriel professionnel')}
+                                </label>
+                            </div>
+                            <div className="formGroup field">
+                                <input className="formField" {...register("companyName")} />
+                                <label htmlFor="entityName" className="formLabel">
+                                    {t('Nom de votre organisation')}
+                                </label>
+                            </div>
+                            <div className="formGroup field">
+                                <input className="formField" {...register("companyAddress")} />
+                                <label htmlFor="proAddress" className="formLabel">
+                                    {t('Adresse de votre organisation')}
+                                </label>
+                            </div>
+                            <div className="formGroup">
+                                <input type="checkbox" id="consentCGV" onChange={handleLegalRep} />
+                                <label htmlFor="consentCGV">
+                                    {t("Je ne suis pas le représentant légal de l'organisation")}
+                                </label>
+                            </div>
+                            {!legalRep && <div className="formGroup field">
+                                <input className="formField" required {...register("legalRepresentative")} />
+                                <label htmlFor="name" className="formLabel">
+                                    {t('Nom du représentant légal')}
+                                </label>
+                            </div>}
+                            {!legalRep && <div className="formGroup field">
+                                <input type="email" className="formField" {...register("emailLegalRepresentative")} />
+                                <label htmlFor="proEmail" className="formLabel">
+                                    {t('Courriel du représentant légal')}
+                                </label>
+                            </div>}
+                            <div className="formGroup">
+                                <input type="checkbox" id="consentCGV" required />
+                                <label htmlFor="consentCGV">
+                                    {t("Je consens à rejoindre DasesLab, et m’engage à respecter sa")}
+                                    <Link to="/mentions">
+                                        {t("charte d'utilisation")}
+                                    </Link>
+                                </label>
+                            </div>
+                            <input type="submit" className="button blackButton" value="Confirmer mon identité" />
+                        </form>
+                    </section>}
+                {!!vcDone && <section className="main onboardingStep onboardingUserInfos">
+                    <button className="closeButton fixed" onClick={handleClose}>
+                        {t('Fermer')}
+                    </button>
+                    <h1>
+                        {t('Votre laissez-passer est créé')}
+                    </h1>
+                    <p>{t("Vous pouvez maintenant utiliser les services du portail")}</p>
+                    <button className="button blackButton" onClick={handleClose}>
+                        {t('Retourner sur le Dashboard')}
+                    </button>
+                    <p /><p />
+                    <p>{t('Vous pouvez télécharger votre laissez-passer pour le conserver en local')}</p>
+                    <button className="button blackButton" onClick={handleVc}>
+                        {t('Télécharger votre laissez-passer')}
+                    </button>
+                </section>}
+            </div>
+        </section>
 
-                )
+    )
 }
 
-                export default VerifiableCredential;
+export default VerifiableCredential;
