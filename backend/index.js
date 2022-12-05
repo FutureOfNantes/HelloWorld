d5457465df152c8b25fc521b4334897dd47a597c
Perrin <perrin.grandne@protonmail.com>
2022-12-05 16:26:01 +0100

    change backend host

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ dotenv.config();
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: 'https://daseslab.on.fleek.co',
   credentials: true,
 }));
 
--- client/src/features/api.js
+++ client/src/features/api.js
@@ -1,12 +1,13 @@
 // For Production Deployment : If you want to use locally => Comment the line, else uncomment
-const environment = 'https://dases-proto.herokuapp.com'
+// const environment = 'https://dases-proto.herokuapp.com'
+const environment = 'https://daseslab.onrender.com'
 
 // For Local Deployment : If you want to use locally => Uncomment, else comment the line
-// const environment = 'http://localhost:5000'
+// const environment = 'http://localhost:8080'
 
 export const URL_SD = environment + '/sd'
 export const URL_USER = environment + '/users'
 export const URL_NONCE = environment + '/nonce'
 export const URL_VERIFY = environment + '/verify'
 
-export const URL_VISIONS = 'https://staging.visionstrust.com/decentralized/px'
+export const URL_VISIONS = 'https://visionstrust.com/decentralized/px'
